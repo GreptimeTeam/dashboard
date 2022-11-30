b543f7a7142f45a403edffed429812dfb5351641
alili <122499469@qq.com>
2022-11-30 10:22:43 +0800

    refactor(store): code-run

--- src/store/modules/code-run/index.ts
+++ src/store/modules/code-run/index.ts
@@ -4,11 +4,9 @@ import { defineStore } from 'pinia'
 
 const useCodeRunStore = defineStore('codeRun', {
   state: () => ({
-    usedCode: <any>[],
-    runResult: <any>[],
-    resultTabIndex: <any>[],
+    resultIndex: <number>-1,
+    results: <any>[],
     activeTabKey: <number>0,
-    activeTabData: <any>{},
     oneLog: <any>{},
     logArray: <any>[],
   }),
@@ -44,10 +42,13 @@ const useCodeRunStore = defineStore('codeRun', {
           content: 'success',
         })
         if (runCode.toLocaleLowerCase().substring(0, 6) === 'select') {
-          this.usedCode.push(runCode)
-          this.runResult.push(res)
-          this.resultTabIndex.push(this.resultTabIndex.length)
-          this.activeTabKey = this.resultTabIndex.length - 1
+          this.resultIndex += 1
+          this.results.push({
+            ...res,
+            index: this.resultIndex,
+            code: runCode,
+          })
+          this.activeTabKey = this.results.length - 1
         }
         // todo: can we combine next two logs into one line code?
         this.oneLog = {
@@ -59,6 +60,25 @@ const useCodeRunStore = defineStore('codeRun', {
           log: error,
         }
       }
+    },
+    setActiveTabKey(index: number) {
+      this.activeTabKey = index
+    },
+    removeResult(index: number) {
+      if (this.results.length === 1) {
+        this.clearResult()
+        return
+      }
+      const indexToRemove = this.results.findIndex((item: any) => item.index === index)
+      this.results.splice(indexToRemove, 1)
+      if (index === this.activeTabKey) {
+        this.activeTabKey = this.results[indexToRemove].index
+      }
+    },
+    clearResult() {
+      this.results = []
+      this.resultIndex = -1
+      this.activeTabKey = 0
     },
   },
 })
