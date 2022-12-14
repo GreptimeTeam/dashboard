eaba59ed7f1d41468c667c2ea570f68b1a2b87cf
wonderful <122499469@qq.com>
2022-12-14 17:04:24 +0800

    build(axios): add axios.d.ts

---
+++ src/api/axios.d.ts
@@ -0,0 +1,8 @@
+/* eslint-disable no-shadow */
+import { AxiosRequestConfig } from 'axios'
+
+declare module 'axios' {
+  export interface AxiosRequestConfig<D = any> {
+    traceTimeStart: number
+  }
+}
