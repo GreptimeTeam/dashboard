4c6d3d508c43bd629082468e25d1efcb6576b692
zonahe <heye@greptime.com>
2023-01-03 10:24:20 +0800

    fix: delete space from api

--- src/api/editor.ts
+++ src/api/editor.ts
@@ -1,19 +1,19 @@
 import axios from 'axios'
 
 export function getSqlResult(code: any) {
-  return axios.post(`/api/v1/sql?sql= ${code}`)
+  return axios.post(`/api/v1/sql?sql=${code}`)
 }
 
 export function postScripts(name: string, code: any) {
-  return axios.post(`/api/v1/scripts?name= ${name}`, code)
+  return axios.post(`/api/v1/scripts?name=${name}`, code)
 }
 
 export function getTables() {
   const code = 'show tables'
-  return axios.post(`/api/v1/sql?sql= ${code}`)
+  return axios.post(`/api/v1/sql?sql=${code}`)
 }
 
 export function fetchOneTable(tableName: any) {
   const code = `desc table ${tableName}`
-  return axios.post(`/api/v1/sql?sql= ${code}`)
+  return axios.post(`/api/v1/sql?sql=${code}`)
 }
