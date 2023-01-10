076b6e932c184059c77510acd5dd87f313582693
zonahe <heye@greptime.com>
2023-01-10 17:16:59 +0800

    fix(script): edit logs content

--- src/views/dashboard/data-explorer/components/log.vue
+++ src/views/dashboard/data-explorer/components/log.vue
@@ -9,9 +9,10 @@ a-card(:bordered="false")
     a-list-item(v-for="item of logs[codeType]" :key="item")
       a-tooltip(:content="item.error ? item.error : item.runCode")
         .log-error(v-if="item.error") {{$t('dataExplorer.error')}}: {{item.error}}
-        a-space.log-space(v-if="item.execution_time_ms" size="large")
+        a-space.log-space(v-else-if="item.execution_time_ms" size="large")
           template(#split)
             a-divider(direction="vertical")
+          div(v-if="codeType==='python'") {{ $t('dataExplorer.runScript', {name: item.name}) }}
           div {{ $tc('dataExplorer.executed', item.result.length, { length: item.result.length })}}
           div {{ $t('dataExplorer.results') }}: 
             span(v-for="(oneResult, index) of item.result" :key="index") {{ oneResult.records >= 0 ? $tc('dataExplorer.select', oneResult.records, {records: oneResult.records}) : $tc('dataExplorer.affected', oneResult.affectedRows, {record: oneResult.affectedRows}) }}; 
@@ -19,7 +20,6 @@ a-card(:bordered="false")
           div {{ $t('dataExplorer.network', {time: item.networkTime - item.execution_time_ms})}}
           div {{ $t('dataExplorer.total', {time: item.networkTime}) }}
           div(v-if="codeType==='sql'") {{ $t('dataExplorer.code', {code: item.runCode}) }}
-          div(v-else) {{ $t('dataExplorer.runScript', {name: item.name}) }}
         a-space.log-space(v-else size="large")
           template(#split)
             a-divider(direction="vertical")
