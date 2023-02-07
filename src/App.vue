3d5c72e7a5c301fdffd956cbbe361a42c773bfee
ZonaHe <heye@greptime.com>
2023-02-07 18:02:12 +0800

    refactor: add url for prod mode

--- src/App.vue
+++ src/App.vue
@@ -26,10 +26,10 @@
 
   const { setRole } = useUserStore()
   const { isCloud, guideModal } = storeToRefs(useAppStore())
-  const { fetchDatabases, setDefaultDatabase } = useAppStore()
+  const { fetchDatabases } = useAppStore()
   // TODO: is there a better way to do this?
   if (import.meta.env.MODE === 'development' || import.meta.env.MODE === 'production') {
-    fetchDatabases('dev')
+    fetchDatabases('notCloud')
   } else {
     isCloud.value = true
     setRole('cloud')
