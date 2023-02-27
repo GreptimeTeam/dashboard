57cc51ce420c4484c9b0cf4791015b3adbb1e247
ZonaHe <heye@greptime.com>
2023-02-27 16:53:29 +0800

    style: layout min-width; log ellipsis and style;

--- src/views/dashboard/query/index.vue
+++ src/views/dashboard/query/index.vue
@@ -19,31 +19,3 @@ a-layout.layout
   // TODO: add more code type in the future if needed
   codeType.value = 'sql'
 </script>
-
-<style lang="less" scoped>
-  .left-side {
-    flex: 1;
-    overflow: auto;
-  }
-
-  .right-side {
-    width: 280px;
-    margin-left: 16px;
-  }
-</style>
-
-<style lang="less" scoped>
-  // responsive
-  .mobile {
-    .container {
-      display: block;
-    }
-
-    .right-side {
-      // display: none;
-      width: 100%;
-      margin-left: 0;
-      margin-top: 16px;
-    }
-  }
-</style>
