5580a7d5db7336307b17596873a85cf57641e862
ZonaHe <heye@greptime.com>
2023-03-01 14:01:11 +0800

    feat: add new tab for empty result
    Add new tab for empty result #149

--- src/views/dashboard/modules/data-view/components/data-grid.vue
+++ src/views/dashboard/modules/data-view/components/data-grid.vue
@@ -1,17 +1,27 @@
 <template lang="pug">
-a-spin(style='width: 100%')
-  a-table(:columns='gridColumn', :data='gridData', :pagination='pagination')
+a-card(:bordered="false")
+  template(#title)
+    a-space(size="mini")
+      img(src="/src/assets/images/table.png" height="20")
+      | {{$t('dataExplorer.table')}}
+  a-spin(style='width: 100%')
+    a-table(:columns='gridColumn', :data='gridData', :pagination='pagination')
 </template>
 
 <script lang="ts" setup>
+  import { useCodeRunStore } from '@/store'
+
   const { currentResult } = storeToRefs(useCodeRunStore())
+
   const pagination = {
     'total': currentResult.value.records.rows.length,
     'show-page-size': true,
   }
 
   const gridColumn = computed(() => {
-    return currentResult.value.records.schema.column_schemas.map((column: any) => {
+    const { schema } = currentResult.value.records
+    if (!schema) return []
+    return schema.column_schemas.map((column: any) => {
       return {
         title: column.name,
         dataIndex: column.name.replace(/\./gi, '-'),
--- src/views/dashboard/modules/data-view/index.vue
+++ src/views/dashboard/modules/data-view/index.vue
@@ -2,22 +2,10 @@
 a-tabs.result-tabs(type="rounded" lazy-load :active-key="activeTabKey[codeType]" @tab-click="tabClick" @delete="deleteTab" editable :animation="true")
   template(#extra)
     a-button(@click="clearResults()" status="danger") {{$t('dataExplorer.clear')}}
-  a-tab-pane(v-for="(item, index) of results[codeType]" :key="item.key" 
-  :title="`${$t('dataExplorer.result')} ${item.key + 1}`" closable) 
+  a-tab-pane(v-for="(item, index) of results[codeType]" :key="item.key" :title="`${$t('dataExplorer.result')} ${item.key + 1}`" closable) 
     a-space(direction="vertical" fill size="small")
-      a-card(:bordered="false")
-        template(#title)
-          a-space(size="mini")
-            img(src="/src/assets/images/table.png" height="20")
-            | {{$t('dataExplorer.table')}}
-        DataGrid
-      a-card(:bordered="false")
-        template(#title)
-          a-space(size="mini")
-            svg.card-icon
-              use(href="#chart")
-            | {{$t('dataExplorer.chart')}}
-        DataChart
+      DataGrid
+      DataChart
 </template>
 
 <script lang="ts" name="DataView" setup>
