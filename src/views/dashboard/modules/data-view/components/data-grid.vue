b5470e09d586ce28e5c01bed1927f9d887f0267d
DiamondMofeng <363034937@qq.com>
2023-04-04 14:28:24 +0800

    feat: add time column formatter

--- src/views/dashboard/modules/data-view/components/data-grid.vue
+++ src/views/dashboard/modules/data-view/components/data-grid.vue
@@ -1,16 +1,28 @@
 <template lang="pug">
-a-card(:bordered="false")
+a-card(:bordered='false')
   template(#title)
-    a-space(size="mini")
+    a-space(size='mini')
       svg.icon-20
-        use(href="#table")
-      | {{$t('dataExplorer.table')}}
+        use(href='#table')
+      | {{ $t('dataExplorer.table') }}
   a-spin(style='width: 100%')
-    a-table(:columns='gridColumn', :data='gridData', :pagination='pagination')
+    a-table(:data='gridData', style='margin-top: 30px')
+      template(#columns)
+        template(v-for='schema in gridColumns', :key='schema.name')
+          template(v-if='timeColumnNames.includes(schema.title)')
+            a-table-column(
+              :title='generateTimeColumnHeader(schema.title)',
+              :data-index='timeColumnFormatMap[schema.title] ? `${schema.title}__FORMATTED` : schema.title'
+            )
+          template(v-else)
+            a-table-column(:title='schema.title', :data-index='schema.title')
 </template>
 
 <script lang="ts" setup>
   import { useCodeRunStore } from '@/store'
+  import { dateTypes } from '@/views/dashboard/config'
+  import { Tooltip } from '@arco-design/web-vue'
+  import { IconHistory } from '@arco-design/web-vue/es/icon'
 
   const { currentResult } = storeToRefs(useCodeRunStore())
 
@@ -19,7 +31,7 @@ a-card(:bordered="false")
     'show-page-size': true,
   }
 
-  const gridColumn = computed(() => {
+  const gridColumns = computed(() => {
     const { schema } = currentResult.value.records
     if (!schema) return []
     return schema.column_schemas.map((column: any) => {
@@ -30,6 +42,12 @@ a-card(:bordered="false")
     })
   })
 
+  const timeColumnNames = computed(() => {
+    return currentResult.value.records.schema.column_schemas
+      .filter((column) => dateTypes.includes(column.data_type))
+      .map((column) => column.name)
+  })
+
   const gridData = computed(() => {
     return currentResult.value.records.rows.map((row: any) => {
       const tempRow: any = {}
@@ -37,7 +55,60 @@ a-card(:bordered="false")
         const columnName = currentResult.value.records.schema.column_schemas[index].name.replace(/\./gi, '-')
         tempRow[columnName] = item
       })
+
+      // pre-calculated formatted time column data
+      timeColumnNames.value.forEach((columnName) => {
+        tempRow[`${columnName}__FORMATTED`] = new Date(tempRow[columnName]).toLocaleString()
+      })
       return tempRow
     })
   })
+
+  /**
+   * use extra state to store the formatted time column data
+   */
+  const timeColumnFormatMap = ref(
+    Object.fromEntries(
+      timeColumnNames.value.map((columnName) => {
+        return [columnName, false]
+      })
+    )
+  )
+
+  // return any because a-table-column's title prop was defined to be a string, although it also accepts a component
+  const generateTimeColumnHeader = computed(() => (title: string): any => {
+    return h('span', null, [
+      h('span', null, title),
+      h(
+        Tooltip,
+        {
+          content: 'Format time',
+          placement: 'top',
+        },
+        {
+          default: () =>
+            h(IconHistory, {
+              style: {
+                marginLeft: '5px',
+                cursor: 'pointer',
+              },
+              onClick: () => {
+                timeColumnFormatMap.value[title] = !timeColumnFormatMap.value[title]
+              },
+            }),
+        }
+      ),
+    ])
+  })
+
+  // /**
+  //  *
+  //  */
+  // const handleformatTimeColumn = (comulnName: string) => {
+  //   const { schema } = currentResult.value.records
+  //   if (!schema) return []
+  //   const columnSchema = schema.column_schemas.find((column: any) => column.name === comulnName)
+  //   if (!columnSchema) return []
+  //   return columnSchema.data_type === 'timestamp' ? 'YYYY-MM-DD HH:mm:ss' : 'YYYY-MM-DD'
+  // }
 </script>
