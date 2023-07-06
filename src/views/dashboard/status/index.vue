d05caeb9b27dc886d8d1319d756d11344cac836d
ZonaHe <zonahe@qq.com>
2023-07-06 15:20:30 +0800

    refactor(status): add textcopyable and edit style

--- src/views/dashboard/status/index.vue
+++ src/views/dashboard/status/index.vue
@@ -1,46 +1,30 @@
 <template lang="pug">
-a-modal.guide-modal(
-  v-model:visible="statusModal"
-  :mask-closable="false"
-  :ok-text="$t('status.confirm')"
-  :hide-cancel="true"
-  :closable="false"
-  @ok="handleOk"
-)
-  template(#title)
-    div {{ $t('status.info') }}
-    svg.guide-banner
-      use(href="#banner")
-
-a-layout.layout
+a-layout.layout.status
   a-layout-content
     a-card(v-if="statusInfoRef && statusInfoRef.length > 0" title="GreptimeDB Status")
       template(#extra)
         a-button(type="text" @click="refreshStatus") refresh
-        a-typography
-          a-typography-paragraph(copyable @copy="copyToClipboard")
+        TextCopyable(copyTooltip="Copy to Clipboard" :data="JSON.stringify(statusData)" :showData="false")
       a-descriptions(bordered :column="2")
         a-descriptions-item(v-for="item of statusInfoRef" :label="item[0]")
           a-tag {{ item[1] }}
     a-empty(v-else)
-      template(#extra)
+      template(#image)
+        svg.icon-32
+          use(href="#empty")
+      | Status is not supported until GreptimeDB v0.3.1
 </template>
 
 <script lang="ts" setup name="Status">
   import { getStatus } from '@/api/status'
 
   const statusModal = ref()
+  const statusData = ref()
   const statusInfoRef = ref()
 
   const refreshStatus = async () => {
-    const status = await getStatus()
-    statusInfoRef.value = Object.entries(status)
-  }
-
-  const copyToClipboard = async () => {
-    const status = await getStatus()
-    navigator.clipboard.writeText(JSON.stringify(status))
-    statusModal.value = true
+    statusData.value = await getStatus()
+    statusInfoRef.value = Object.entries(statusData.value)
   }
 
   const handleOk = () => {
@@ -52,9 +36,24 @@ a-layout.layout
   })
 </script>
 
-<style lang="stylus" rel="stylesheet/stylus" scoped>
-  .arco-layout-content
-    background-color #fff
-    border-radius 10px
-    padding 10px 20px
+<style lang="less" scoped>
+  .arco-layout-content {
+    background-color: #fff;
+    border-radius: 10px;
+    padding: 10px 20px;
+  }
+
+  :deep(.arco-tag) {
+    border-radius: 6px;
+  }
+
+  .status {
+    .arco-typography {
+      line-height: 32px;
+
+      &.copy {
+        margin-bottom: 0;
+      }
+    }
+  }
 </style>
