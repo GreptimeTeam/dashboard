89c8e5fa4d286197a780ca1eddad782db7b8dee1
ZonaHe <zonahe@qq.com>
2023-11-07 14:18:20 +0800

    style(logs): edit style

--- src/views/dashboard/modules/log.vue
+++ src/views/dashboard/modules/log.vue
@@ -1,37 +1,39 @@
 <template lang="pug">
 a-list-item.smaller-divider
   template(v-if="log.type !== 'python' && !log.error" #actions)
-    a-button(@click="openEditor") {{ codeType }}
-  a-space(direction="vertical")
-    a-space.code
+    a-button.play(type="text" @click="openEditor")
+      template(#icon)
+        icon-play-arrow
+  a-space(direction="vertical" fill :size="0")
+    .code
       a-tooltip(v-if="log.error" :content="log.error")
         div {{ log.error }}
-      a-popover(v-else-if="log.type !== 'python'")
+      a-popover(v-else-if="log.type !== 'python'" content-class="code-popup")
         template(#content)
-          div
-            a-list(size="small" :split="false" :bordered="false")
-              a-list-item(v-if="log.type === 'promql'" v-for="(value, name) in log.promInfo")
-                span.width-35 {{ name }}
-                a-typography-text.ml-4(code) {{ value }}
-              a-list-item(v-else) {{ log.codeInfo }}
+          a-list(size="small" :split="false" :bordered="false")
+            a-list-item(v-if="log.type === 'promql'" v-for="(value, name) in log.promInfo")
+              span.width-35 {{ name }}
+              a-typography-text.ml-4(code) {{ value }}
+            a-list-item(v-else)
+              a-typography-text.popup {{ log.codeInfo }}
         div {{ log.codeInfo }}
       .script(v-else)
         div(v-if="hasExecutionTime") {{ $t('dashboard.runScript', { name: log.codeInfo }) }}
         div(v-else) {{ $t('dashboard.saveName', { name: log.codeInfo }) }}
-    a-space.info
-      icon-check-circle.success-color(v-if="!log.error")
+    a-space.info(fill)
+      icon-check-circle.success-color.icon-14(v-if="!log.error")
       icon-close-circle.danger-color(v-else)
       .start-time
         | {{ log.startTime }}
-      a-space.result(v-if="!log.error" :size="4")
-        div(v-if="hasExecutionTime")
+      a-space.result(v-if="!log.error" fill :size="4")
+        a-space(v-if="hasExecutionTime" :size="0")
+          template(#split) ;
           span(v-for="(oneResult, index) of log.results" :key="index") {{ oneResult.records >= 0 ? $tc('dashboard.select', oneResult.records, { records: oneResult.records }) : $tc('dashboard.affected', oneResult.affectedRows, { record: oneResult.affectedRows }) }}
         .total-time(v-if="hasExecutionTime")
-          a-popover
+          a-popover(content-class="total-time-popover")
             template(#content)
               div {{ $t('dashboard.executeTime', { time: log.execution_time_ms }) }}
               div {{ $t('dashboard.network', { time: log.networkTime - log.execution_time_ms }) }}
-              div {{ $t('dashboard.total', { time: log.networkTime }) }}
             div {{ `in ${log.networkTime} ms` }}
 </template>
 
@@ -72,3 +74,58 @@ a-list-item.smaller-divider
     }
   }
 </script>
+
+<style lang="less" scoped>
+  .code {
+    font-family: monospace;
+    color: var(--main-font-color);
+    font-size: 14px;
+
+    div {
+      text-overflow: ellipsis;
+      white-space: nowrap;
+      overflow: hidden;
+    }
+  }
+
+  .popup {
+    color: var(--small-font-color);
+    border: 0;
+    border-radius: 4px;
+    font-size: 12px;
+    font-family: monospace;
+    white-space: pre-wrap;
+  }
+
+  .info {
+    > :first-child {
+      font-size: 14px;
+    }
+  }
+
+  .total-time {
+    background: var(--th-bg-color);
+    border-radius: 4px;
+    padding: 0 2px;
+  }
+
+  .result {
+    & :deep(.arco-space-item-split) {
+      margin-right: 4px !important;
+    }
+  }
+
+  .play {
+    font-size: 17px;
+  }
+</style>
+
+<style lang="less">
+  .total-time-popover {
+    font-size: 12px;
+    padding: 4px 10px;
+    .arco-popover-content {
+      color: var(--small-font-color);
+    }
+  }
+</style>
--- src/views/dashboard/modules/logs-layout.vue
+++ src/views/dashboard/modules/logs-layout.vue
@@ -40,12 +40,36 @@ a-card.logs-card(:bordered="false")
   .logs-card {
     height: 100%;
   }
-
+  :deep(.arco-list) {
+    border-radius: 0;
+  }
   :deep(.arco-list-content) {
     border-top: 1px solid var(--border-color);
     border-bottom: 1px solid var(--border-color);
+    width: calc(100% - 32px);
+    display: flex;
+    flex-direction: column-reverse;
   }
   :deep(.arco-list-item:not(:last-child)) {
     border-color: var(--border-color);
+  }
+
+  :deep(.arco-list-item:last-child) {
+    border-bottom: 1px solid var(--border-color);
+  }
+
+  :deep(.arco-list-item:first-child) {
+    border-bottom: none;
+  }
+  :deep(.arco-list-item-action) {
+    width: 32px;
+  }
+
+  :deep(.arco-list-item-main) {
+    width: calc(100% - 32px);
+  }
+
+  :deep(.arco-list-small .arco-list-content-wrapper .arco-list-content > .arco-list-item) {
+    padding: 4px 10px 4px 20px;
   }
 </style>
