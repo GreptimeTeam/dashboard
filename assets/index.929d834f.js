b3e582285c393f7d51bbb1dd6beddb6d93c5412d
ZonaHex <ZonaHex@users.noreply.github.com>
2023-11-17 08:16:36 +0000

    deploy: 4a883b582eed200bcdefe3bce318ff22bcf3256b

---
+++ assets/index.929d834f.js
@@ -0,0 +1,1 @@
+import{d as o,D as r,C as a}from"./arco.f573c5ac.js";import{i as s,g as n}from"./vue.67fd90f7.js";const d=o({__name:"index",setup(c){const e=s(),t=n().params.path;return e.replace({path:t}),(u,m)=>(a(),r("div"))}});export{d as default};
