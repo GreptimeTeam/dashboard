a3c568f894c25e0ba4a5d2e3e8d389c7d3c46c2e
ZonaHe <zonahe@qq.com>
2023-12-21 17:58:45 +0800

    refactor: rename logo img

--- src/components/navbar/index.vue
+++ src/components/navbar/index.vue
@@ -2,7 +2,7 @@
 a-layout.navbar
   a-layout-header
     .logo-space
-      img.logo-text-img(alt="logo" src="/src/assets/images/logo.svg")
+      img.logo-text-img(alt="logo" src="/src/assets/images/logo-text.svg")
   a-layout-content
     .new-query
       a-button(type="primary" @click="openQuery")
