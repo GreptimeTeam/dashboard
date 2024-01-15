60e50492735326c27f524dae8d84506022e60e4c
ZonaHe <zonahe@qq.com>
2024-01-15 14:19:44 +0800

    style: use png for logo (#404)

--- src/components/navbar/index.vue
+++ src/components/navbar/index.vue
@@ -2,8 +2,7 @@
 a-layout.navbar
   a-layout-header
     .logo-space
-      svg.logo-text-img
-        use(href="#logo-db")
+      img.logo-text-img(alt="logo" src="/src/assets/images/logo-text.png")
   a-layout-content
     .new-query
       a-button(type="primary" @click="openQuery")
