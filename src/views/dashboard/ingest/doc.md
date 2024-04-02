921c6725d62892e41eb91e101c47e2375a41d518
ZonaHe <zonahe@qq.com>
2024-04-02 18:18:57 +0800

    feat(ingest): info drawer

---
+++ src/views/dashboard/ingest/doc.md
@@ -0,0 +1,32 @@
+### About Line Protocol
+
+The general syntax of an InfluxDB Line Protocol data point is as follows:
+
+```
+<table>[,<tag_key>=<tag_value>[,...]] <field_key>=<field_value>[,<field_key>=<field_value>[,...]] [timestamp]
+```
+
+- `<table>`: The name of the Table.
+- `<tag_key>=<tag_value>`: Tags provide metadata for the data point. They are optional but useful for filtering and grouping data.
+- `<field_key>=<field_value>`: Fields represent the actual data values associated with the table.
+- `[timestamp]`: Optional. If not provided, the server's current time will be used.
+
+### Example
+
+Let's say we want to write a data point representing CPU usage:
+
+- Table: `cpu_usage`
+- Tags: `host=server1,region=us-west`
+- Fields: `usage_user=80,usage_system=10`
+
+The Line Protocol for this data point would look like:
+
+```
+cpu_usage,host=server1,region=us-west usage_user=80,usage_system=10
+```
+
+If you want to include a timestamp (e.g., 1621401600000000000, which represents June 1, 2021, at 12:00:00 AM UTC), you can add it at the end:
+
+```
+cpu_usage,host=server1,region=us-west usage_user=80,usage_system=10 1621401600000000000
+```
--- src/views/dashboard/ingest/top-bar.vue
+++ src/views/dashboard/ingest/top-bar.vue
@@ -1,18 +1,41 @@
 <template lang="pug">
 a-space.top-bar
-  a-button(
-    type="primary"
-    size="small"
-    :loading="loading"
-    :disabled="disabled"
-    @click="clickSubmit"
-  ) Write
-  a-select(
-    v-model="precision"
-    style="width: 140px"
-    size="small"
-    :options="precisionOptions"
+  a-space(:size="15")
+    a-select(
+      v-model="precision"
+      style="width: 140px"
+      size="small"
+      :options="precisionOptions"
+    )
+    a-button(
+      type="primary"
+      size="small"
+      :loading="loading"
+      :disabled="disabled"
+      @click="clickSubmit"
+    ) Write
+  a-tooltip(
+    content="About InfluxDB Line Protocol"
+    position="tr"
+    trigger="hover"
+    mini
   )
+    a-button(type="outline" size="small" @click="visible = !visible")
+      template(#icon)
+        svg.icon-16.brand-color(v-if="!visible")
+          use(href="#document")
+        icon-close.icon-16(v-else)
+a-drawer.ingest(
+  v-model:visible="visible"
+  placement="right"
+  :width="437"
+  :footer="false"
+  :header="false"
+)
+  .markdown-container.ingest
+    MarkdownRender(:md="doc")
+  | To learn more about influxdb line protocol on GreptimeDB, visit our
+  a(href="https://docs.greptime.com/user-guide/write-data/influxdb-line" target="_blank") documentation
 </template>
 
 <script lang="ts" setup name="TopBar">
@@ -21,8 +44,8 @@ a-space.top-bar
     loading: boolean
   }>()
   const emits = defineEmits(['submit'])
-
   const { precision } = storeToRefs(useIngestStore())
+  const visible = ref(false)
 
   const clickSubmit = () => {
     emits('submit', precision.value)
@@ -46,6 +69,8 @@ a-space.top-bar
       label: 'Seconds',
     },
   ]
+
+  const doc = Object.entries(import.meta.glob('./doc.md', { as: 'raw', eager: true }))[0][1]
 </script>
 
 <style lang="less" scoped>
@@ -61,6 +86,59 @@ a-space.top-bar
     :deep(.arco-select-view-single) {
       background: transparent;
       border-color: var(--border-color);
+    }
+  }
+</style>
+
+<style lang="less">
+  .arco-drawer-container.ingest {
+    .arco-drawer {
+      top: 54px;
+      border: 1px solid var(--border-color);
+      border-radius: 4px;
+      height: calc(100% - 54px - 26px);
+      margin-right: 20px;
+      box-shadow: 0px 2px 20px 0px var(--box-shadow-color);
+      .arco-drawer-header {
+        border: none;
+      }
+    }
+    .arco-drawer-body {
+      color: var(--color-gray-3);
+      font-size: 13px;
+      a {
+        color: var(--brand-color);
+        font-size: 13px;
+        margin-left: 4px;
+      }
+    }
+
+    .markdown-container.ingest {
+      margin-bottom: 10px;
+      h3 {
+        margin: 10px 0 10px;
+      }
+      p {
+        margin: 10px 0;
+        font-size: 13px;
+      }
+      code {
+        font-size: 12px;
+        color: var(--color-red-3);
+        background: var(--color-code-bg);
+        border-radius: var(--border-radius-s);
+        border: 1px solid var(--color-dark-primary-1);
+      }
+      pre {
+        margin: 0;
+      }
+      ul {
+        margin: 10px 0;
+      }
+      li {
+        margin: 0px 0;
+        font-size: 13px;
+      }
     }
   }
 </style>
