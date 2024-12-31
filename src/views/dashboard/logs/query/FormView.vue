9bacfe8baa4b6cf4af82473bfd167b7401fe7be4
zzzdong <kuwater@163.com>
2024-12-31 14:35:55 +0800

    feat(log-query): support display json field (#471)

--- src/views/dashboard/logs/query/FormView.vue
+++ src/views/dashboard/logs/query/FormView.vue
@@ -10,14 +10,24 @@ a-descriptions(layout="vertical" bordered :column="1")
 <script setup name="FormView" type="ts">
   import useLogQueryStore from '@/store/modules/logquery';
 
+  const displayValue = (val, type) => {
+    switch (type) {
+      case 'json':
+        return JSON.stringify(val, null, 2)
+      default:
+        return val
+    }
+  }
+
   const props = defineProps(['data'])
   const { columns } = storeToRefs(useLogQueryStore())
   const formData = computed(() => {
     return Object.keys(props.data).filter((v) => v !== 'key').map((v) => {
+      const columnType = columns.value.filter(c => c.name === v)[0]?.data_type;
       return {
         label: v,
-        type: columns.value.filter(c => c.name === v)[0]?.data_type,
-        value: props.data[v]
+        type: columnType,
+        value: displayValue(props.data[v], columnType)
       }
     })
   })
