3c165b491b7fac8131c99a7e45cd345450fe0117
scl <sunchang_long@163.com>
2025-02-21 18:33:54 +0800

    feat: basic workable

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,7 +1,12 @@
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
+pub mod app_plugin;
+
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_process::init())
+        .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_updater::Builder::new().build())
+        .plugin(app_plugin::init())
         .setup(|app| {
             if cfg!(debug_assertions) {
                 app.handle().plugin(
@@ -15,3 +20,5 @@ pub fn run() {
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
+
+// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
--- src/tauri/checkupdate.js
+++ src/tauri/checkupdate.js
@@ -1,32 +1,41 @@
 import { check } from '@tauri-apps/plugin-updater'
 import { relaunch } from '@tauri-apps/plugin-process'
+import { confirm } from '@tauri-apps/plugin-dialog'
 
-async function checkForUpdatesOnStart() {
+export default async function checkUpdate() {
+  console.log('checkForUpdatesOnStart')
   try {
     const update = await check()
+    console.log('check result', update)
     if (update) {
-      console.log(`Found update ${update.version} from ${update.date} with notes ${update.body}`)
-      let downloaded = 0
-      let contentLength = 0
+      const confirmation = await confirm(
+        `Found update ${update.version} from ${update.date} with notes ${update.body}`,
+        { title: 'Update', kind: 'info' }
+      )
+      if (confirmation) {
+        console.log(`Found update ${update.version} from ${update.date} with notes ${update.body}`)
+        let downloaded = 0
+        let contentLength = 0
 
-      await update.downloadAndInstall((event) => {
-        switch (event.event) {
-          case 'Started':
-            contentLength = event.data.contentLength
-            console.log(`Started downloading ${event.data.contentLength} bytes`)
-            break
-          case 'Progress':
-            downloaded += event.data.chunkLength
-            console.log(`Downloaded ${downloaded} from ${contentLength}`)
-            break
-          case 'Finished':
-            console.log('Download finished')
-            break
-        }
-      })
+        await update.downloadAndInstall((event) => {
+          switch (event.event) {
+            case 'Started':
+              contentLength = event.data.contentLength
+              console.log(`Started downloading ${event.data.contentLength} bytes`)
+              break
+            case 'Progress':
+              downloaded += event.data.chunkLength
+              console.log(`Downloaded ${downloaded} from ${contentLength}`)
+              break
+            case 'Finished':
+              console.log('Download finished')
+              break
+          }
+        })
 
-      console.log('Update installed')
-      await relaunch()
+        console.log('Update installed')
+        await relaunch()
+      }
     } else {
       console.log('No updates available.')
     }
@@ -36,4 +45,4 @@ async function checkForUpdatesOnStart() {
 }
 
 // 在应用启动时调用
-checkForUpdatesOnStart()
+// checkForUpdatesOnStart()
