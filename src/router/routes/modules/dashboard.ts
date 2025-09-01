e43e64499d02125c0076c5d6f0d885ccd9233348
scl <sunchang_long@163.com>
2025-09-01 15:14:23 +0800

    fix: cache cause problem

--- src/router/routes/modules/dashboard.ts
+++ src/router/routes/modules/dashboard.ts
@@ -35,6 +35,7 @@ const DASHBOARD: AppRouteRecordRaw = {
         requiresAuth: false,
         icon: 'Node16',
         roles: ['admin', 'cloud'],
+        ignoreCache: true,
       },
     },
     {
